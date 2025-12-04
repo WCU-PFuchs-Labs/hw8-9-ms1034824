ed9470f267ca1e5b6c75dde8c0e42edc784281b1
ms1034824 <ms1034824@wcupa.edu>
2025-12-03 21:40:46 -0500

    Attempt 1 update gptree and generation

--- GPTree.java
+++ GPTree.java
@@ -1,49 +1,41 @@
 import java.util.*;
-public class GPTree implements Collector, Comparable<GPTree>, Cloneable{
+
+public class GPTree implements Collector, Comparable<GPTree>, Cloneable {
     private Node root;
     private ArrayList<Node> crossNodes;
-    
-    
-    /**
-     * @param - node The node to be collected.
-     * TODO: implement this method
-     */
-    public void evalFitness(DataSet dataset){
-        for(int i = 0; i < dataset.getRows().size(); i++){
-            
-            DataRow.getIndependentVariables();
-        }
+    private double fitness; //add
+
+    public static void main (String [] args){
+        System.out.println("hello");
     }
+
+    public void evalFitness(DataSet dataset){
+        double sum = 0.0;
+        for (DataRow row : dataset.getRows()) {
+            double[] datasetX = row.getIndependentVariables();
+            double predicted = eval(datasetX);
+            double actual = row.getDependentVariable();
+            double diff = predicted - actual;
+            sum += Math.pow(diff, 2);
+        }
+        this.fitness = sum;
+    }
+
+    public double getFitness() {
+        return fitness;
+    }
+
     public void collect(Node node) {
         if(!node.isLeaf()){
             crossNodes.add(node);
         }
     }
-    
-    
-    // DO NOT EDIT code below for Homework 8. 
-    // If you are doing the challenge mentioned in 
-    // the comments above the crossover method
-    // then you should create a second crossover
-    // method above this comment with a slightly 
-    // different name that handles all types
-    // of crossover.
-    
-    
-    /**
-     * This initializes the crossNodes field and
-     * calls the root Node's traverse method on this
-     * so that this can collect the Binop Nodes.
-     */
+
     public void traverse() {
         crossNodes = new ArrayList<Node>();
         root.traverse(this);
     }
-    
-    /**
-     * This returns a String with all of the binop Strings
-     * separated by semicolons
-     */
+
     public String getCrossNodes() {
         StringBuilder string = new StringBuilder();
         int lastIndex = crossNodes.size() - 1;
@@ -55,50 +47,60 @@ public class GPTree implements Collector, Comparable<GPTree>, Cloneable{
         string.append(crossNodes.get(lastIndex));
         return string.toString();
     }
-   
-    
-    /**
-     * this implements left child to left child
-     * and right child to right child crossover.
-     * Challenge: additionally implement left to 
-     * right child and right to left child crossover.
-     */
+
     public void crossover(GPTree tree, Random rand) {
-        // find the points for crossover
         this.traverse();
         tree.traverse();
         int thisPoint = rand.nextInt(this.crossNodes.size());
         int treePoint = rand.nextInt(tree.crossNodes.size());
         boolean left = rand.nextBoolean();
-        // get the connection points
         Node thisTrunk = crossNodes.get(thisPoint);
         Node treeTrunk = tree.crossNodes.get(treePoint);
 
-        
         if(left) {
             thisTrunk.swapLeft(treeTrunk);
-            
         } else {
             thisTrunk.swapRight(treeTrunk);
         }
-        
     }
 
     GPTree() { 
         root = null; 
     }    
-    
+
     public GPTree(NodeFactory n, int maxDepth, Random rand) {
         root = n.getOperator(rand);
         root.addRandomKids(n, maxDepth, rand);
     }
-    
+
     public String toString() { 
         return root.toString(); 
     }
-    
+
     public double eval(double[] data) { 
         return root.eval(data); 
     }
-    
+
+    @Override
+    public int compareTo(GPTree other) {
+        return Double.compare(this.fitness, other.fitness);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || !(o instanceof GPTree)) return false;
+        return this.compareTo((GPTree) o) == 0;
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            GPTree copy = (GPTree) super.clone();
+            copy.root = (Node) this.root.clone();
+            return copy;
+        } catch (CloneNotSupportedException e) {
+            System.out.println("GPTree can't clone.");
+            return null;
+        }
+    }
 }
